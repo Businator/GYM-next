c1a6824da4435fbfcfa934dcc39d13968e621e83
Marat Deniullov <deniullov@gmail.com>
2023-05-24 16:39:14 +0400

    refactor: change style choose section

--- src/pages/main/components/choose/Choose.tsx
+++ src/pages/main/components/choose/Choose.tsx
@@ -9,53 +9,55 @@ import { integral } from "../../../../assets/fonts/fonts";
 
 import styles from "./Choose.module.scss";
 
+const data = [
+  {
+    id: 1,
+    header: "MUSCLE BUILDING",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus repellendus similique vero illum sed totam soluta voluptas",
+    img: dumbbell,
+  },
+  {
+    id: 2,
+    header: "MUSCLE BUILDING",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus repellendus similique vero illum sed totam soluta voluptas",
+    img: exspander,
+  },
+  {
+    id: 3,
+    header: "MUSCLE BUILDING",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus repellendus similique vero illum sed totam soluta voluptas",
+    img: bottle,
+  },
+];
+
 export const Choose = () => {
   return (
     <section>
       <h2 className={integral.className}>WHY CHOOSE US</h2>
       <div className={styles.container}>
-        <div className={styles.card}>
-          <Image alt="dumbbell" src={dumbbell} width={100} height={100} />
-          <h3 className={integral.className}>MUSCLE BUILDING</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-            repellendus similique vero illum sed totam soluta voluptas
-          </p>
-          <div>
-            <LinkWithArrow />
-            <span>
-              <TiFlashOutline />
-            </span>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <Image alt="exspander" src={exspander} width={100} height={100} />
-          <h3 className={integral.className}>MUSCLE BUILDING</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-            repellendus similique vero illum sed totam soluta voluptas
-          </p>
-          <div>
-            <LinkWithArrow />
-            <span>
-              <TiFlashOutline />
-            </span>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <Image alt="bottle" src={bottle} width={100} height={100} />
-          <h3 className={integral.className}>MUSCLE BUILDING</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-            repellendus similique vero illum sed totam soluta voluptas
-          </p>
-          <div>
-            <LinkWithArrow />
-            <span>
-              <TiFlashOutline />
-            </span>
-          </div>
-        </div>
+        {data.map((element) => {
+          return (
+            <div key={element.id} className={styles.card}>
+              <Image
+                alt={element.img.src}
+                src={element.img}
+                width={100}
+                height={100}
+              />
+              <h3 className={integral.className}>{element.header}</h3>
+              <p>{element.description}</p>
+              <div>
+                <LinkWithArrow />
+                <span className={styles.flash}>
+                  <TiFlashOutline />
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
