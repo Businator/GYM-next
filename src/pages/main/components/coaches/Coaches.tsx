13bacce54acb5491c3b39c6e1745650b3b5ecf44
Marat Deniullov <deniullov@gmail.com>
2023-05-25 10:48:49 +0400

    feat: add swiper for section coach

--- src/pages/main/components/coaches/Coaches.tsx
+++ src/pages/main/components/coaches/Coaches.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-import thor from "./assets/Thor.jpg";
-import capitanAmerica from "./assets/Capitan-America.jpg";
-import superman from "./assets/Sups.jpg";
-import capitanMarvel from "./assets/Capitan-Marvel.jpeg";
-import widow from "./assets/Widow.jpeg";
-import wonderWoman from "./assets/Wonder-woman.jpeg";
-import Image from "next/image";
-
 import styles from "./Coaches.module.scss";
 import { integral } from "'@/assets/fonts/fonts'";
+import { SwiperCoaches } from "./components/SwiperCoaches/SwiperCoaches";
 
 export const Coaches = () => {
   return (
@@ -19,24 +12,7 @@ export const Coaches = () => {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
         voluptate atque sapiente quia.
       </p>
-      <div className={styles.container}>
-        <div>
-          <Image src={thor} alt="Thor" />
-          <p>CROSSFIT COACH</p>
-        </div>
-        <div>
-          <Image src={capitanAmerica} alt="Capitan America" />
-          <p>CROSSFIT COACH</p>
-        </div>
-        <div>
-          <Image src={superman} alt="Superman" />
-          <p>CROSSFIT COACH</p>
-        </div>
-        <div>
-          <Image src={wonderWoman} alt="Wonder Woman" />
-          <p>CROSSFIT COACH</p>
-        </div>
-      </div>
+      <SwiperCoaches />
     </section>
   );
 };
---
+++ src/pages/main/components/coaches/components/SwiperCoaches/SwiperCoaches.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/scss";
+import "swiper/scss/pagination";
+import Image from "next/image";
+
+import thor from "../../assets/images/Thor.jpg";
+import capitanAmerica from "../../assets/images/Capitan-America.jpg";
+import superman from "../../assets/images/Sups.jpg";
+import capitanMarvel from "../../assets/images/Capitan-Marvel.jpeg";
+import widow from "../../assets/images/Widow.jpeg";
+import wonderWoman from "../../assets/images/Wonder-woman.jpeg";
+
+import styles from "./SwiperCoaches.module.scss";
+
+const coachesList = [
+  {
+    id: 1,
+    name: "Thor",
+    img: thor,
+    spec: "crossfit",
+  },
+  {
+    id: 2,
+    name: "Wonder Woman",
+    img: wonderWoman,
+    spec: "crossfit",
+  },
+  {
+    id: 3,
+    name: "Superman",
+    img: superman,
+    spec: "powerlifting",
+  },
+  {
+    id: 4,
+    name: "Black Widow",
+    img: widow,
+    spec: "stretching",
+  },
+
+  {
+    id: 5,
+    name: "Capitan Marvel",
+    img: capitanMarvel,
+    spec: "open gym",
+  },
+  {
+    id: 6,
+    name: "Capitan America",
+    img: capitanAmerica,
+    spec: "crossfit",
+  },
+];
+
+export const SwiperCoaches = () => {
+  return (
+    <div className={styles.swiperContainer}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+          bulletClass: styles.bullet,
+          bulletActiveClass: styles.bulletActive,
+        }}
+        modules={[Pagination]}
+      >
+        {coachesList.map((coach) => {
+          return (
+            <SwiperSlide className={styles.card}>
+              <Image src={coach.img} alt={coach.name} />
+              <div>
+                <h3>{coach.name}</h3>
+                <p>{coach.spec.toUpperCase()}</p>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
