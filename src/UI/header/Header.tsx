e8149b918a1ef67d6fcce5bb73247f267b111648
Marat Deniullov <deniullov@gmail.com>
2023-05-29 10:45:01 +0400

    feat: add mobile menu

--- src/UI/header/Header.tsx
+++ src/UI/header/Header.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import Link from "next/link";
+import { capitalize } from "@mui/material";
 import { Logo } from "../logo/Logo";
+import { MobileMenu } from "./components/MobileMenu/MobileMenu";
+import { MessengerAndSearchMenu } from "./components/MessengerAndSearchMenu/MessengerAndSearchMenu";
 import styles from "./Header.module.scss";
-import { BsMessenger, BsSearch } from "react-icons/bs";
+
+const pageList = ["home", "shop", "app", "blog", "contact"];
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <Logo />
-      <ul>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/shop"}>Shop</Link>
-        <Link href={"/App"}>App</Link>
-        <Link href={"/blog"}>Blog</Link>
-        <Link href={"/contact"}>Contact</Link>
-      </ul>
-      <div className={styles.icons}>
-        <span>
-          <BsSearch />
-        </span>
-        <span>
-          <BsMessenger />
-        </span>
-      </div>
+      {document.documentElement.scrollWidth < 768 ? (
+        <MobileMenu pageList={pageList} />
+      ) : (
+        <>
+          <ul>
+            {pageList.map((page) => {
+              return (
+                <li key={page}>
+                  <Link href={page}>{capitalize(page)}</Link>
+                </li>
+              );
+            })}
+          </ul>
+          <MessengerAndSearchMenu />
+        </>
+      )}
     </header>
   );
 };
---
+++ src/UI/header/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Menu, MenuItem, MenuList, capitalize } from "@mui/material";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import { MessengerAndSearchMenu } from "../MessengerAndSearchMenu/MessengerAndSearchMenu";
+
+import styles from "./MobileMenu.module.scss";
+
+const menuStyle = {
+  "& .MuiBackdrop-root": {
+    background:
+      "linear-gradient(90deg, rgba(23,23,23,1) 0%, rgba(0,0,0,0.6010416666666667) 50%, rgba(23,23,23,1) 100%)",
+  },
+  "& .MuiMenu-paper": {
+    width: 150,
+    backgroundColor: "#D6FD51",
+    boxShadow: "0px 0px 60px 0px #D6FD51",
+  },
+};
+
+export const MobileMenu = ({ pageList }: { pageList: string[] }) => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <>
+      <Menu
+        anchorOrigin={{ horizontal: "right", vertical: "top" }}
+        open={openMenu}
+        onClick={() => setOpenMenu((prev) => !prev)}
+        sx={menuStyle}
+      >
+        <MenuList>
+          {pageList.map((page) => {
+            return (
+              <MenuItem key={page}>
+                <Link href={page}>{capitalize(page)}</Link>
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Menu>
+      <div className={styles.buttonContainer}>
+        <button onClick={() => setOpenMenu((prev) => !prev)}>
+          <AiOutlineMenu />
+        </button>
+        <MessengerAndSearchMenu />
+      </div>
+    </>
+  );
+};
