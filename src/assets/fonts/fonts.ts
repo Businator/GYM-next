cdb9523d7460720a347949034f0b34277296fe13
Marat Deniullov <deniullov@gmail.com>
2023-06-01 13:16:18 +0400

    refactor: delete open sans font

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,10 +1,7 @@
 import localFont from "next/font/local";
-import { Open_Sans } from "next/font/google";
 
 const integral = localFont({
   src: "../fonts/IntegralCF-Medium.woff2",
 });
 
-const openSans = Open_Sans({ subsets: ["cyrillic", "latin"] });
-
-export { integral, openSans };
+export { integral };
--- src/pages/main/components/hero/components/HeroCards/HeroCards.tsx
+++ src/pages/main/components/hero/components/HeroCards/HeroCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SwiperSlide } from "swiper/react";
 
 import styles from "./HeroCards.module.scss";
-import { integral, openSans } from "'@/assets/fonts/fonts'";
+import { integral } from "'@/assets/fonts/fonts'";
 
 export const HeroCards = () => {
   return (
@@ -11,23 +11,17 @@ export const HeroCards = () => {
         <h1 className={integral.className}>
           WORK WITH <br /> PROFFESOINALS
         </h1>
-        <p className={openSans.className}>
-          Achieve your goals and become the best version of yourself
-        </p>
+        <p>Achieve your goals and become the best version of yourself</p>
       </SwiperSlide>
       <SwiperSlide className={styles.card}>
         <h1 className={integral.className}>WORK ALONE</h1>
-        <p className={openSans.className}>
-          Achieve your goals and become the best version of yourself
-        </p>
+        <p>Achieve your goals and become the best version of yourself</p>
       </SwiperSlide>
       <SwiperSlide className={styles.card}>
         <h1 className={integral.className}>
           WORK WITH <br /> OUR APPLICATION
         </h1>
-        <p className={openSans.className}>
-          Achieve your goals and become the best version of yourself
-        </p>
+        <p>Achieve your goals and become the best version of yourself</p>
       </SwiperSlide>
     </>
   );
