da5c8a82ba620c86b7311431a51c4f3489804629
Marat Deniullov <deniullov@gmail.com>
2023-06-13 10:34:36 +0400

    refactor: cgange swiper in app

--- src/pages/app/components/appSwiper/AppSwiper.tsx
+++ src/pages/app/components/appSwiper/AppSwiper.tsx
@@ -2,9 +2,10 @@
 
 import React from "react";
 import { Pagination } from "swiper";
-import { SwiperSlide } from "swiper/react";
+import { SwiperSlide, useSwiper } from "swiper/react";
 import Image from "next/image";
 import { MySwiper } from "'@/components/MySwiper/MySwiper'";
+import { Button } from "'@/UI/button/Button'";
 
 import img1 from "./assets/image/app-swiper-card-image-1.jpg";
 import img2 from "./assets/image/app-swiper-card-image-2.jpg";
@@ -33,13 +34,14 @@ export const AppSwiper = () => {
   return (
     <div className={styles.container}>
       <MySwiper slidesPerView={1} modules={[Pagination]} spaceBetween={50}>
-        {contentList.map((item, index) => {
+        {contentList.map((item, index, arr) => {
           return (
             <SwiperSlide key={index} className={styles.card}>
               <Image src={item.img} alt="" />
               <div className={styles.content}>
                 <span>{item.title.toUpperCase()}</span>
                 <span>{item.descripion.toUpperCase()}</span>
+                {index === arr.length - 1 && <Button>START NOW</Button>}
               </div>
               <div className={styles.block}></div>
             </SwiperSlide>
